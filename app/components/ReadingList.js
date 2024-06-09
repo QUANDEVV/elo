8da531a7632719d826e41c5788f773b97a77ab1e
QUANDEVV <vercelploy@gmail.com>
2024-06-09 16:25:49 +0300

    done

--- app/components/ReadingList.js
+++ app/components/ReadingList.js
@@ -3,33 +3,33 @@ import React from "react";
 const ReadingList = ({ list, removeFromReadingList }) => {
   return (
     <div className="px-6">
-      <h2 className="mb-4 text-2xl font-bold text-center">Teacher's List</h2>
-      <div className="flex flex-wrap gap-4 sm:grid sm:grid-cols-2 sm:gap-4 mx:22">
+      <h2 className="mb-6 ml-[240px] text-[1.625rem] font-bold text-[#335C6E] sm:ml-0">
+        Teacher's List
+      </h2>
+      <div className="flex flex-wrap justify-center gap-4 mx-auto  grid-cols-6  sm:grid sm:grid-cols-2 sm:gap-4 max-w-[1360px]">
         {list.map((book, index) => (
-          <div
-            key={index}
-            className="bg-white rounded-lg shadow-lg flex flex-col w-[150px] cursor-pointer"
-          >
+          <div key={index} className="  flex flex-col w-[151px] cursor-pointer">
             <div className="mb-4">
               <img
                 src={book.coverPhotoURL}
                 alt={`${book.title} cover`}
-                className="w-full"
+                className="w-[150px] h-[150px] object-cover "
               />
             </div>
+
             <div className="flex-grow">
-              <p className="text-base leading-relaxed font-weight-800">
+              <p className="text-base font-bold leading-relaxed text-gray-800">
                 {book.title}
               </p>
             </div>
             <div>
-              <p className="text-gray-600 text-[12px] leading-relaxed">
+              <p className="text-gray-500 text-[0.8125rem] font-semibold leading-6">
                 by {book.author}
               </p>
             </div>
             <button
               onClick={() => removeFromReadingList(book)}
-              className="py-2 mt-4 text-white bg-red-500 rounded"
+              className="py-2 mt-4 text-white bg-[#53C2C2] hover:bg-[#28B8B8] rounded-lg shadow-md"
             >
               Remove
             </button>
